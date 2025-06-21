10ac89d1f27a6f06fc974bf598da8ebb4ebcae7b
leuim <jassim.mohammed48@gmail.com>
2025-06-21 13:43:52 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const PORT = procces.env.PORT ? procces.env.PORT : '3000';
+
+//middle wares
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// DB connection
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', ()=>{
+    console.log(`Connected to: ${mongoose.connection.name}`);
+})
+
+app.get('/', async (req,res)=>{
+    res.render('index.ejs')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port: ${PORT}`);
+})
