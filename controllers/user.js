b9ab103857a5db68bc0d4d6f591e6e41038d4872
leuim <jassim.mohammed48@gmail.com>
2025-06-22 02:14:30 +0300

    fix the password validation condition

--- controllers/user.js
+++ controllers/user.js
@@ -11,23 +11,23 @@ router.post('/sign-up', async (req, res) => {
     try {
         const userInDatabase = await User.findOne({ username: req.body.username });
         if (userInDatabase) {
-            return res.send('User is in the database already')
+            return res.send('User is in the database already');
         }
-        if (!req.body.password === req.body.confirmPassword) {
-            return res.send('Passwords Dont match!')
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.send('Passwords Dont match!');
         }
         if (!validator.isEmail(req.body.email)) {
-            return res.send('Not a valid email!')
+            return res.send('Not a valid email!');
         }
-        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
-        req.body.password = hashedPassword
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+        req.body.password = hashedPassword;
 
-        const user = await User.create(req.body)
+        const user = await User.create(req.body);
         req.session.userId = user._id;
         res.redirect('/')
     } catch (error) {
         console.log(error);
-        res.send('Error')
+        res.send('Error');
     }
 });
 
