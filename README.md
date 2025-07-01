7c121959f63a663f95b705b5fd32179b4c25e9f9
leuim <jassim.mohammed48@gmail.com>
2025-07-01 11:15:46 +0300

    finishing touches to the styles added a background image and a logo

---
+++ README.md
@@ -0,0 +1,4 @@
+# SubTracker
+![Placeholder for we site page]()
+
+SubTracker is an application that allows you to track you subscriptions across the board for instance online subs like streaming services or actual utilities like electrecity and so on. it also allows you to create, update and delete transactions of a subscription. SubTracker is a unified place to track all your sub related expanses. 
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT ? process.env.PORT : "3000";
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -33,7 +34,6 @@ app.use(expressLayouts);
 app.set('layout', './layouts/layout');
 app.use(passUserToView)
 app.use(passCurrentPathToView)
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(connectFlash())
 app.use(passFlashMessageToView)
 // DB connection
@@ -52,8 +52,7 @@ app.use('/auth', authCtrl);
 app.use('/subscriptions', isSignedIn, subcriptionsCtrl)
 app.use(`/transactions`, isSignedIn, transactionCtrl)
 app.get('/', async (req,res)=>{
-    res.render('index.ejs', {title:'SubTracker'});
-    console.log(req.path);
+    res.render('index.ejs');
 })
 
 app.listen(PORT,()=>{
